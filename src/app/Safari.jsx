01e339a57e3b658050bdf150f629929a1f86be0d
emirkaraca195-bit <emirkaraca195@gmail.com>
2026-09-07 09:35:12 +0400

    Remove title attribute from iframe
    Removed title attribute from iframe in Safari component.

--- src/app/Safari.jsx
+++ src/app/Safari.jsx
@@ -334,7 +334,6 @@ export function Safari({ initialUrl = "https://www.google.com/webhp?igu=1", wind
           onLoad={handleIframeLoad}
           className={`w-full h-full border-none hide-scrollbar ${isDragging || isResizing ? "pointer-events-none" : ""}`}
           referrerPolicy="no-referrer"
-          title="Safari Browser"
         />
       </div>
 
